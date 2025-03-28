d6fcf10b7b0ed0bf92ee92c3fa5c2bc6560f4d1d
emowang <yimengpuppet@gmail.com>
2025-03-27 22:37:33 -0700

    update

--- assets/scripts/GameLobby.ts
+++ assets/scripts/GameLobby.ts
@@ -1,9 +1,8 @@
-import { _decorator, Component, instantiate, Label, Layout, Node, UITransform, Widget } from 'cc';
+import { _decorator, Component, instantiate, Label, Color, Node, Sprite } from 'cc';
 const { ccclass, property } = _decorator;
 
 // TODO: 从配置中获取
 const FONTSIZE = 18
-const ROOMHEIGHT = 40
 
 // TODO: 从接口获取数据
 const lobbyData = {
@@ -38,6 +37,8 @@ const lobbyData = {
 @ccclass('GameLobby')
 export class GameLobby extends Component {
     roomList = []
+    curRoomId: number = null
+
     onLoad() {
         console.log(this.node, 'GameLobby 场景加载成功')
     }
@@ -54,10 +55,11 @@ export class GameLobby extends Component {
     initLobbyTableUI() {
         if (this.roomList.length === 0) return;
 
-        const sv = this.node.getChildByName('roomScrollView');
         // add titles
+        const sv = this.node.getChildByName('roomScrollView');
         Object.keys(this.roomList[0]).forEach((columnName: string) => {
             let titleNode = new Node(columnName);
+            titleNode.name = columnName
             titleNode.addComponent(Label).string = columnName
             titleNode.getComponent(Label).fontSize = FONTSIZE
             sv.getChildByName('titles').addChild(titleNode)
@@ -66,8 +68,10 @@ export class GameLobby extends Component {
         // TODO: add button
 
         // add RoomList
+        let roomSample = sv.getChildByName('view').getChildByName('content').getChildByName('roomSample')
         this.roomList.forEach((room: any) => {
-            let roomNode = instantiate(sv.getChildByName('view').getChildByName('roomSample'))
+            let roomNode = instantiate(roomSample)
+            roomNode.name = room.id.toString()
             Object.keys(room).forEach((columnName: string) => {
                 let detailNode = new Node(room + columnName)
                 detailNode.addComponent(Label).string = room[columnName]
@@ -75,7 +79,26 @@ export class GameLobby extends Component {
                 roomNode.addChild(detailNode)
             })
             sv.getChildByName('view').getChildByName('content').addChild(roomNode)
+            roomNode.on(Node.EventType.TOUCH_END, (e: any) => this.clickRoom(e, room.id), this)
         });
+        roomSample.active = false
+    }
+
+    clickRoom(e: any, roomId: number) {
+        console.log('点击了房间:', roomId)
+        const selColor = new Color(255, 182, 193, 140);
+        const unselColor = new Color(140, 140, 140, 140);
+        let svContent = this.node.getChildByName('roomScrollView').getChildByName('view').getChildByName('content')
+
+        if (this.curRoomId === roomId) return
+
+        if (this.curRoomId !== null) {
+            svContent.getChildByName(this.curRoomId.toString()).getComponent(Sprite).color = unselColor;
+        }
+        this.curRoomId = roomId;
+        svContent.getChildByName(this.curRoomId.toString()).getComponent(Sprite).color = selColor;
+        // TODO: 跳转到 RoomScene
+
     }
 
 }
